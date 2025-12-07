c307770fe984fed3f842fc0cfb9fb06ebbeb4673
Romeo Julian B Jackson <r.jackson@vtileuven.be>
2025-12-07 12:08:18 +0100

    Add stub TemplatePreview component

---
+++ src/components/TemplatePreview.tsx
@@ -0,0 +1,10 @@
+/**
+ * Stub component - Template preview removed since we standardized on healthcare.
+ */
+export function TemplatePreview(_props: {
+    template?: string;
+    className?: string;
+}) {
+    // No-op: template preview is no longer used
+    return null;
+}
